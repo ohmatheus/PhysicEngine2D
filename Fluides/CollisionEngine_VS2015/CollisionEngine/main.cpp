35528cb55c128552bb56ce13f4cb3305984cfe93
Marech <max.marechal@gmail.com>
2017-06-02 11:47:38 +0200

    SFML 2 in place before replacing code

--- Fluides/CollisionEngine_VS2015/CollisionEngine/main.cpp
+++ Fluides/CollisionEngine_VS2015/CollisionEngine/main.cpp
@@ -12,13 +12,33 @@
 #include "Scenes/SceneBouncingPolys.h"
 #include "Scenes/SceneSpheres.h"
 #include "SceneFluid.h"
-
+#include <SFML\Graphics.hpp>
 
 /*
 * Entry point
 */
 int _tmain(int argc, char** argv)
 {
+
+	/*sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+	sf::CircleShape shape(100.f);
+	shape.setFillColor(sf::Color::Green);
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+		window.clear();
+		window.draw(shape);
+		window.display();
+	}*/
+
+
 	InitApplication(1260, 768, 50.0f);
 
 	gVars->pSceneManager->AddScene(new CSceneFluid());
